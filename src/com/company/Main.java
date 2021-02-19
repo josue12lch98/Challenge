dd3659d2a8991294475f67ce912db47be9176dc0
Josué López <jlopezc@pucp.edu.pe>
2021-02-19 17:00:42 -0500

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,29 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola");
-        System.out.println("¿Hay sol? (true/false)");
+        System.out.println("Hola, llenarás el formulario de https://apprater.net/add/ siguiendo este programa");
         Scanner sc = new Scanner(System.in);
-        Boolean isSol = sc.nextBoolean();
+        String [] labels = {"Your name","Product Tittle", "Email Address", "Product URL","Add another Product URL"};
+        int index=1;for (String label : labels ){ System.out.println(index++ + " " +label);}
+        int label = sc.nextInt();String [] selectors = {"css","name", "id", "xpath"};
+        index=1;for (String selector : selectors ){ System.out.println(index++ + " " +selector);}
+        int selector = sc.nextInt();
+        System.out.println("Escribir valor de la entrada");
+        String value = sc.nextLine() +sc.nextLine(); label--;selector--;
+        System.out.println("Se ejecutará el siguiente código: website.sendText("+ "\"" +labels[label] + "\",\""+selectors[selector]+ "\",\"user-submitted-"+selectors[selector]+"\",\""+value+"\",\"" );
 
-        System.out.println("¿Qué temperatura tenemos?(int)");
-
-        int temperatura = sc.nextInt();
-
-        System.out.println("¿Tenemos ropa de playa?(true/false)");
-        Boolean isRopaDePlaya = sc.nextBoolean();
-
-
-        if (isSol) {
-            if (temperatura < 23) {
-                System.out.println(" Dar un paseito  ");
-            } else if (temperatura <= 25 && temperatura >= 23) {
-                System.out.println(" Vamos a la playa ");
-
-            } else {
-                System.out.println(" Vamos a la playa y no olvidar la sombrilla que quemamos ");
-            }
-        }
 
 
     }
